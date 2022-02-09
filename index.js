e1ae202561787c175a0d4357096edb51937c4ef7
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-09 10:49:50 -0500

    commit

--- index.js
+++ index.js
@@ -1,95 +1,51 @@
 /** GLOBALS **/
-const allPomodoros = document.querySelector('#all-pomodoros');
+const base_url = "http://localhost:3000/data";
+let allPomodoros = document.querySelector('#all-pomodoros');
+let form = document.querySelector('#add-pomodoros-form');
 
 
-/** EVENT LISTENER **/
-document.querySelector('#add-pomodoros-form').addEventListener('submit', handleSubmit)
-
-/** EVENT HANDLER **/
-function handleSubmit(e) {
-    e.preventDefault();
-    let pomodoroObj = {
-        topic:e.target.topic.value, 
-        category:e.target.category.value,
-        due:e.target.due.value,
-        pomodoroNum: 1
-    };
-    addNewPomodoro(pomodoroObj);
-}
+/* WHEN THE DOM LOADS */
+document.addEventListener('DOMContentLoaded', () => {
+    getPomodoros(); //initialize
+    form.addEventListener('submit', (event) => {
+        handleSubmit(event);
+    })
+})
 
 /** FETCH REQUESTS **/
+
+
 function getPomodoros() {
-    fetch("http://localhost:3000/data")
+    fetchData(base_url)
     .then(res => res.json())
-    .then(pomodoroData => pomodoroData.map(pomodoro => renderOnePomodoro(pomodoro)))
+    .then(pomodoroData => { 
+        pomodoroData.map(pomodoro => render(pomodoro))
+    })
 }
-getPomodoros(); //initialize
 
+/** RENDER **/
+function render(pomodoro) {
 
-/** RENDERS **/
-function renderOnePomodoro(pomodoro) {
-
-    const card = document.createElement('div');
-
+    let card = document.createElement('div');
     card.innerHTML = `
         <p>
             <b>Topic: ${pomodoro.topic}</b>
         </p>
         <p>Category: ${pomodoro.category}</p>
         <p>Due Date: ${pomodoro.due}</p>
-        <p id="increase"">
-            &#127813 <span>${pomodoro.pomodoroNum}</span>
-        </p>
-        <p id="remove">Complete</p>
+        <p>${pomodoro.pomodoroNum} pomodoros</p>
+        <button class="buttons" id=${pomodoro.id}>&#127813</button>
     `;
 
     allPomodoros.appendChild(card);
 
     //Event listener to increase pomodoro count
-    card.querySelector("#increase").addEventListener("click", (e) => {
-        e.preventDefault;
-        pomodoro.pomodoroNum+=1;
-        card.querySelector("span").textContent = pomodoro.pomodoroNum;
-        increasePomodoroCount(pomodoro);
-    })
+    card.querySelector(".buttons").addEventListener("click", (event) => {
+        //console.log(event.target.dataset)
+        //increasePomodoroCount(event)
+    }
+        //pomodoro.pomodoroNum+=1;
+        //card.querySelector("span").innerText = pomodoro.pomodoroNum;
+    )   
 
-    //Event listener to remove completed pomodoro
-    card.querySelector("#remove").addEventListener('click', (e) => {
-        e.preventDefault();
-        removePomodoro(pomodoro);
-    })
-
-
-}
-
-function addNewPomodoro(pomodoroObj) {
-    fetch("http://localhost:3000/data", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify(pomodoroObj)
-    })
-}
-
-function increasePomodoroCount(pomodoroObj) {
-    fetch(`http://localhost:3000/data/${pomodoroObj.id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify(pomodoroObj)
-    })
-}
-
-function removePomodoro(pomodoroObj) {
-    fetch(`http://localhost:3000/data/${pomodoroObj.id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        }
-    })
 }
